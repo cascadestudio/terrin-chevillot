131a32f2fd43c6044f87e39a17b4c0d647dec798
Clément Lapasset <clement.lapasset@gmail.com>
2024-10-02 17:07:16 +0200

    change favicon

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -1,6 +1,7 @@
 import Form from "../form/Form";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer id="contact" className="relative py-8 bg-blue text-white">
       <img
@@ -13,7 +14,11 @@ export default function Footer() {
           <h2 className="font-black uppercase text-[28px] leading-[34px] lg:text-[48px] lg:leading-[55px] xl:text-[63px] xl:leading-[70px]">
             Nous contacter
           </h2>
-          <a href="mailto:terrin.chevillot@gmail.com">
+          <a
+            href="mailto:terrin.chevillot@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             terrin.chevillot@gmail.com
             <img
               src="/link-icon.svg"
@@ -21,7 +26,11 @@ export default function Footer() {
               className="inline-block w-3 h-3 lg:w-4 lg:h-4 ml-3 lg:ml-5 mb-1"
             />
           </a>
-          <a href="tel:06 35 33 35 78">
+          <a
+            href="tel:06 35 33 35 78"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             06 35 33 35 78
             <img
               src="/link-icon.svg"
@@ -29,7 +38,11 @@ export default function Footer() {
               className="inline-block w-3 h-3 lg:w-4 lg:h-4 ml-3 lg:ml-5 mb-1"
             />
           </a>
-          <a href="https://www.google.fr/maps/place/99+Chem.+de+la+M%C3%BBre,+13015+Marseille/@43.3647287,5.3698352,863m/data=!3m2!1e3!4b1!4m6!3m5!1s0x12c9ea988606afc1:0xd452049b671da5e!8m2!3d43.3647248!4d5.3724101!16s%2Fg%2F11c17vpymg?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D">
+          <a
+            href="https://www.google.fr/maps/place/99+Chem.+de+la+M%C3%BBre,+13015+Marseille/@43.3647287,5.3698352,863m/data=!3m2!1e3!4b1!4m6!3m5!1s0x12c9ea988606afc1:0xd452049b671da5e!8m2!3d43.3647248!4d5.3724101!16s%2Fg%2F11c17vpymg?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             99 Chemin de la Mûre <br /> 13015 Marseille
             <img
               src="/link-icon.svg"
@@ -37,7 +50,11 @@ export default function Footer() {
               className="inline-block w-3 h-3 lg:w-4 lg:h-4 ml-3 lg:ml-5 mb-1"
             />
           </a>
-          <a href="https://www.facebook.com/terrin.chevillot">
+          <a
+            href="https://www.facebook.com/terrin.chevillot"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Facebook
             <img
               src="/link-icon.svg"
@@ -54,7 +71,7 @@ export default function Footer() {
             className="h-[22px] col-span-4 lg:col-span-1"
           />
           <p className="col-start-1 col-span-3 row-start-2 self-start whitespace-nowrap lg:self-end lg:col-start-2 lg:row-start-1 lg:col-span-1">
-            © TC Bâtiment 2024
+            © TC Bâtiment {currentYear}
           </p>
           <a
             className="col-start-5 hover:underline col-span-3 row-start-1 justify-self-end lg:col-start-3 lg:justify-self-start lg:col-span-1"
@@ -64,7 +81,12 @@ export default function Footer() {
           </a>
           <p className="col-span-4 justify-self-end whitespace-nowrap lg:col-span-2 lg:col-start-6">
             Site réalisé par le{" "}
-            <a href="https://www.cascadestudio.fr" className="hover:underline">
+            <a
+              href="https://www.cascadestudio.fr"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Studio Cascade
             </a>
           </p>
