817d70b17d0b54b742239099e65282680af4ca40
Clément Lapasset <clement.lapasset@gmail.com>
2024-10-14 11:02:49 +0200

    remove slug in schema

--- src/sanity/schemaTypes/projectType.js
+++ src/sanity/schemaTypes/projectType.js
@@ -7,21 +7,13 @@ import {
 
 export const projectType = defineType({
   name: "project",
-  title: "Projets - Seuls les 4 premiers seront affichés sur le site",
+  title: "Projets",
   type: "document",
   icon: ProjectsIcon,
   fields: [
     defineField({
       name: "title",
       type: "string",
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: "slug",
-      type: "slug",
-      options: {
-        source: "title",
-      },
       validation: (Rule) => Rule.required(),
     }),
     defineField({
